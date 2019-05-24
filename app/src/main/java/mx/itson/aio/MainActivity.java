7960a23fa76225721c84cb7ac792598533dcd13b
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2019-05-24 00:11:54 -0700

    Last update

--- app/src/main/java/mx/itson/aio/MainActivity.java
+++ app/src/main/java/mx/itson/aio/MainActivity.java
@@ -7,8 +7,6 @@ import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
